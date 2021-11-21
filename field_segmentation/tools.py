89010829bb16e59841d84edbf22366e539ddccab
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2021-11-21 10:27:28 +0100

    add code for downloading the runswift field segmentation dataset from our backup location

--- field_segmentation/tools.py
+++ field_segmentation/tools.py
@@ -3,8 +3,8 @@ from urllib.request import urlretrieve
 from urllib.error import HTTPError, URLError
 
 
-# TODO rename and unify this with other locations where stuff is downloaded directly
-def download_bhuman2019(origin, target):
+def download_function(origin, target):
+
     def dl_progress(count, block_size, total_size):
         print('\r', 'Progress: {0:.2%}'.format(min((count * block_size) / total_size, 1.0)), sep='', end='', flush=True)
 
@@ -27,3 +27,17 @@ def download_bhuman2019(origin, target):
         if Path(target).exists():
             Path(target).unlink()
         raise
+
+
+def download_runswift_segmentation_dataset():
+    data_root_path = "../data_segmentation"
+    data_root_path = Path(data_root_path).resolve()
+
+    download_function("https://logs.naoth.de/Experiments/RunswiftDatasets/Downloads.zip",
+                        f"{data_root_path}/Downloads.zip")
+    download_function("https://arxiv.org/pdf/2108.12809.pdf",
+                        f"{data_root_path}/segmentation_dataset.pdf")
+
+
+if __name__ == '__main__':
+    download_runswift_segmentation_dataset()
