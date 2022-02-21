0e77c719cc418891c9a0e2b50d1996e5e107fbc7
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2022-02-21 12:52:35 +0100

    refactor code for cvat image format to rc labelchallenge format

---
+++ tests/labelchallenge_exporter/challenge_format_converter.py
@@ -0,0 +1,100 @@
+"""
+    Converts cvat image xml files to the format specified in the 2022 label challenge
+"""
+import sys
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+def clipped():
+    xml_label_file = "annotations.xml"
+
+    with open('naoth_annotations_clipped.csv', 'w') as f:
+        # print('This message will be written to a file.', file=f)
+
+        tree = ET.parse(xml_label_file)
+        annotation_root = tree.getroot()
+
+        for image in annotation_root:
+            # ignore meta and version childs in the xml structure
+            if image.tag != "image":
+                continue
+            print(image.attrib)
+            image_name = Path(image.attrib["name"]).name  # TODO parse only the filename not the whole path
+            for bbox in image:
+                if bbox.attrib["occluded"] != "0":
+                    # don't export occluded bounding boxes
+                    continue
+                
+                if bbox.attrib["label"] == "nao":
+                    label = 0
+                    for item in bbox.findall("attribute"):
+                        if item.attrib["name"] == "color":
+                            color = item.text
+                        if item.attrib["name"] == "number":
+                            number = item.text                    
+                elif bbox.attrib["label"] == "ball":
+                    label = 1
+                    color = -1
+                    number = -1                    
+                else:
+                    print("ERROR: there was an unexpected label")
+                    sys.exit(-1)
+
+                xtl = bbox.attrib["xtl"]
+                ytl = bbox.attrib["ytl"]
+                xbr = bbox.attrib["xbr"]
+                ybr = bbox.attrib["ybr"]
+
+                # TODO handle case of bounding box complety outside
+                if float(xtl) < 0: xtl = 0
+                if float(ytl) < 0: ytl = 0
+                if float(xbr) >= 1920: xbr = 1919
+                if float(ybr) >= 1080: ybr = 1080
+
+                print(image_name, label, xtl, ytl, xbr, ybr, color, number, file=f, sep=",")
+
+
+def main():
+    xml_label_file = "annotations.xml"
+
+    with open('naoth_annotations.csv', 'w') as f:
+        # print('This message will be written to a file.', file=f)
+
+        tree = ET.parse(xml_label_file)
+        annotation_root = tree.getroot()
+
+        for image in annotation_root:
+            # ignore meta and version childs in the xml structure
+            if image.tag != "image":
+                continue
+            print(image.attrib)
+            image_name = Path(image.attrib["name"]).name  # TODO parse only the filename not the whole path
+            for bbox in image:
+                if bbox.attrib["occluded"] != "0":
+                    # don't export occluded bounding boxes
+                    continue
+                
+                if bbox.attrib["label"] == "nao":
+                    label = 0
+                    for item in bbox.findall("attribute"):
+                        if item.attrib["name"] == "color":
+                            color = item.text
+                        if item.attrib["name"] == "number":
+                            number = item.text                    
+                elif bbox.attrib["label"] == "ball":
+                    label = 1
+                    color = -1
+                    number = -1                    
+                else:
+                    print("ERROR: there was an unexpected label")
+                    sys.exit(-1)
+
+                xtl = bbox.attrib["xtl"]
+                ytl = bbox.attrib["ytl"]
+                xbr = bbox.attrib["xbr"]
+                ybr = bbox.attrib["ybr"]
+                print(image_name, label, xtl, ytl, xbr, ybr, color, number, file=f, sep=",")
+                
+
+if __name__ == "__main__":
+    clipped()
