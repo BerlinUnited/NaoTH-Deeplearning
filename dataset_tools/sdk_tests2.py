5b8aa7ab13e8939b4b58c11b0bc94ba80ee444b9
Stella Alice Schlotter <stella.schlotter@deepl.com>
2023-03-24 15:49:48 +0100

    feat: use cvat sdk

--- dataset_tools/sdk_tests2.py
+++ dataset_tools/sdk_tests2.py
@@ -2,5 +2,6 @@ from cvat_sdk import make_client, models
 from cvat_sdk.core.proxies.tasks import ResourceType, Task
 
 with make_client(host="https://ball.informatik.hu-berlin.de/", credentials=('stella', 'Simon9247')) as client:
-    tasks = client.tasks.list()
-    #task = client.tasks.export_dataset()
+    task = client.tasks.retrieve(591)
+    #print(tasks)
+    task.export_dataset(format_name="YOLO 1.1", filename="./test.zip")
