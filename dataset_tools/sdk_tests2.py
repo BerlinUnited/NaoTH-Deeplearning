4be6d198881e9c328dcddf78b6a2b027bc3a7c7f
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2023-04-01 20:39:04 +0200

    sdk stuff

--- dataset_tools/sdk_tests2.py
+++ dataset_tools/sdk_tests2.py
@@ -1,7 +1,18 @@
-from cvat_sdk import make_client, models
-from cvat_sdk.core.proxies.tasks import ResourceType, Task
+from cvat_sdk import make_client
 
-with make_client(host="https://ball.informatik.hu-berlin.de/", credentials=('stella', 'Simon9247')) as client:
-    task = client.tasks.retrieve(591)
-    #print(tasks)
-    task.export_dataset(format_name="YOLO 1.1", filename="./test.zip")
+
+def get_projects():
+    with make_client(host="https://ball.informatik.hu-berlin.de/", credentials=('stella', 'Simon9247')) as client:
+        projects = client.projects.list()
+        my_project = projects[0]
+        print(my_project)
+
+
+def download_task(task_id, export_format="YOLO 1.1"):
+    with make_client(host="https://ball.informatik.hu-berlin.de/", credentials=('stella', 'Simon9247')) as client:
+        task = client.tasks.retrieve(task_id)
+        task.export_dataset(format_name=export_format, filename="./test.zip")
+
+
+#download_task(591)
+get_projects()
