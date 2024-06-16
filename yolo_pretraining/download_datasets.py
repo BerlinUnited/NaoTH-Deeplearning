e4e51c9e60e3627a12e3ad2e4c4310571afc3204
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-06-16 17:24:06 +0200

    add output folder argument for downloading dataset function for yolo pretraiing

--- yolo_pretraining/download_datasets.py
+++ yolo_pretraining/download_datasets.py
@@ -10,11 +10,10 @@ from helper import get_file_from_server
 from zipfile import ZipFile
 
 
-def get_dataset(dataset_name):
-    full_path = Path("datasets") / dataset_name
+def get_dataset(dataset_name, output="datasets"):
+    full_path = Path(output) / dataset_name
     if not full_path.exists():
         if not full_path.with_suffix(".zip").exists():
-            remote_path = Path(os.environ["REPL_ROOT"]) / "datasets" / Path(dataset_name).with_suffix(".zip")
             get_file_from_server(f"https://datasets.naoth.de/{dataset_name}.zip", full_path.with_suffix(".zip"))
         
         print("unpack")                      
@@ -26,6 +25,7 @@ def get_dataset(dataset_name):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-ds", "--dataset", required=True)
+    parser.add_argument("-o", "--output", required=False)
 
     args = parser.parse_args()
-    get_dataset(args.dataset)
+    get_dataset(args.dataset, args.output)
