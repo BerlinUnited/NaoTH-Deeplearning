c2a6ea497e6529d0ab4ead0fbd31733f2c4ed662
Thomas Krause <thomaskrause@posteo.de>
2026-03-10 21:08:38 +0100

    Directly conver Keras model

--- balldetection2026/patch_based_training/convert.py
+++ balldetection2026/patch_based_training/convert.py
@@ -1,19 +1,15 @@
-import tensorflow as tf
+import shutil
 from pathlib import Path
-import shutil 
+
+import tensorflow as tf
 
 model_path = "data/BOTTOM/trionda_small.keras"
-temp_export_dir = "data/BOTTOM/temp_saved_model"
 
 model = tf.keras.models.load_model(model_path)
 
-model.export(temp_export_dir)
-
-converter = tf.lite.TFLiteConverter.from_saved_model(temp_export_dir)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 output_file = Path(model_path).with_suffix(".tflite")
 with open(output_file, "wb") as f:
     f.write(tflite_model)
-
-shutil.rmtree(temp_export_dir, ignore_errors=True)
