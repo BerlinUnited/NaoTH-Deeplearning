522e7fbb9c8ccaa351e553054a0837c4ab4b9126
Lennart Keller <bole030@icloud.com>
2026-03-12 10:49:25 +0100

    new classifer model

--- balldetection2026/patch_based_training/train.py
+++ balldetection2026/patch_based_training/train.py
@@ -7,147 +7,9 @@ import sys
 from pathlib import Path
 
 import mlflow
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.layers import (
-    BatchNormalization,
-    Conv2D,
-    Dense,
-    Dropout,
-    Flatten,
-    Input,
-    LeakyReLU,
-    MaxPool2D,
-    ReLU,
-)
-from tensorflow.keras.models import Model
-from tensorflow.keras.regularizers import L1L2, L2
+import tensorflow
 
-
-def build_classifier_cnn_ball_gopen24_functional():
-    input_shape = (16, 16, 1)
-    inputs = Input(shape=input_shape)
-
-    x = Conv2D(16, (5, 5), padding="same", name="Conv2D_1")(inputs)
-    x = ReLU(name="activation_1")(x)
-
-    x = Conv2D(
-        16,
-        (5, 5),
-        padding="valid",
-        strides=(2, 2),
-        kernel_regularizer=L1L2(l1=1e-5, l2=1e-4),
-        name="Conv2D_2",
-    )(x)
-    x = ReLU(name="activation_2")(x)
-
-    x = Conv2D(
-        16,
-        (3, 3),
-        padding="valid",
-        kernel_regularizer=L1L2(l1=1e-5, l2=1e-4),
-        name="Conv2D_3",
-    )(x)
-    x = ReLU(name="activation_3")(x)
-
-    x = Conv2D(
-        16,
-        (3, 3),
-        padding="valid",
-        kernel_regularizer=L1L2(l1=1e-5, l2=1e-4),
-        name="Conv2D_4",
-    )(x)
-    x = ReLU(name="activation_4")(x)
-
-    x = Flatten(name="flatten_1")(x)
-
-    x = Dense(
-        256,
-        activation="relu",
-        kernel_regularizer=L1L2(l1=1e-5, l2=1e-4),
-        bias_regularizer=L2(1e-4),
-    )(x)
-    x = Dropout(0.1)(x)
-
-    x = Dense(
-        32,
-        activation="relu",
-        kernel_regularizer=L1L2(l1=1e-5, l2=1e-4),
-        bias_regularizer=L2(1e-4),
-    )(x)
-
-    outputs = Dense(1, activation="softmax")(x)
-
-    model = Model(inputs=inputs, outputs=outputs)
-
-    return model
-
-
-def make_naoth_detector_generic_functional(
-    input_shape=(16, 16, 1),
-    filters=(8, 8, 16, 16),
-    n_dense=64,
-    regularize=True,
-    final_activation="sigmoid",
-    static_batch_size=None,
-):
-    inputs = Input(shape=input_shape, batch_size=static_batch_size)
-
-    x = Conv2D(filters[0], (3, 3), padding="same", name="Conv2D_1")(inputs)
-    x = BatchNormalization(name="batch_norm_1")(x)
-    x = LeakyReLU(name="activation_1")(x)
-    x = MaxPool2D(pool_size=(2, 2), name="pooling_1")(x)
-
-    x = Conv2D(
-        filters[1],
-        (3, 3),
-        padding="same",
-        name="Conv2D_2",
-        kernel_regularizer=(L1L2(l1=1e-5, l2=1e-4) if regularize else None),
-        bias_regularizer=L2(1e-4) if regularize else None,
-    )(x)
-    x = BatchNormalization(name="batch_norm_2")(x)
-    x = LeakyReLU(name="activation_2")(x)
-    x = MaxPool2D(pool_size=(2, 2), name="pooling_2")(x)
-
-    x = Conv2D(
-        filters[2],
-        (3, 3),
-        padding="same",
-        name="Conv2D_3",
-        kernel_regularizer=(L1L2(l1=1e-5, l2=1e-4) if regularize else None),
-        bias_regularizer=L2(1e-4) if regularize else None,
-    )(x)
-    x = BatchNormalization(name="batch_norm_3")(x)
-    x = LeakyReLU(name="activation_3")(x)
-    x = MaxPool2D(pool_size=(2, 2), name="pooling_3")(x)
-
-    x = Conv2D(
-        filters[3],
-        (2, 2),
-        padding="valid",
-        name="Conv2D_4",
-        kernel_regularizer=(L1L2(l1=1e-5, l2=1e-4) if regularize else None),
-        bias_regularizer=L2(1e-4) if regularize else None,
-    )(x)
-    x = BatchNormalization(name="batch_norm_4")(x)
-    x = LeakyReLU(name="activation_5")(x)
-
-    x = Flatten(name="flatten_1")(x)
-    x = Dense(
-        n_dense,
-        activation="leaky_relu",
-        kernel_regularizer=(L1L2(l1=1e-5, l2=1e-4) if regularize else None),
-        bias_regularizer=L2(1e-4) if regularize else None,
-    )(x)
-
-    if regularize:
-        x = Dropout(0.33)(x)
-
-    outputs = Dense(1, activation=final_activation)(x)
-    model = Model(inputs=inputs, outputs=outputs)
-    return model
-
+from model import build_classifier_cnn_ball_gopen24_functional
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -166,7 +28,7 @@ if __name__ == "__main__":
 
         seed = 42
 
-        (train_ds, val_ds) = tf.keras.utils.image_dataset_from_directory(
+        (train_ds, val_ds) = tensorflow.keras.utils.image_dataset_from_directory(
             f"data/{args.camera}/patches",
             validation_split=0.2,
             subset="both",
@@ -175,7 +37,7 @@ if __name__ == "__main__":
             color_mode="grayscale",
             batch_size=32,
             labels="inferred",
-            label_mode="binary",
+            label_mode="categorical",
             class_names=["noball", "ball"],
         )
 
@@ -190,14 +52,14 @@ if __name__ == "__main__":
         with open(list_path, "w") as f:
             json.dump(list(val_image_names), f, indent=4)
 
-        model = make_naoth_detector_generic_functional()
+        model = build_classifier_cnn_ball_gopen24_functional()
         model.summary()
 
-        model.compile(optimizer="adam", loss="mse", metrics=["mae"])
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
         mlflow.log_param("regularization", True)
 
-        model.fit(train_ds, validation_data=val_ds, epochs=10)
+        model.fit(train_ds, validation_data=val_ds, epochs=2000)
 
         model_path = f"data/{args.camera}/trionda_small_{args.camera}.keras"
         model.save(model_path)
