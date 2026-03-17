2f92d6f3535eb2db633cf359ee51974351592764
Lennart Keller <bole030@icloud.com>
2026-03-17 18:39:56 +0100

    new paths

--- balldetection2026/classifier_patch_based/create_patches.py
+++ balldetection2026/classifier_patch_based/create_patches.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         image_save_dir = Path(f"data/{args.camera}/images/all")
         anno_save_dir = Path(f"data/{args.camera}/annotations/all")
     elif args.predictor == "y":
-        image_save_dir = Path(f"data/{args.camera}/images_not_annotated")
+        image_save_dir = Path(f"data/{args.camera}/images_not_human_proofed")
         anno_save_dir = Path(f"data/yolo/{args.camera}/annotations")
 
     patch_dir = Path(f"data/{args.camera}/patches")
--- balldetection2026/log_iteration.py
+++ balldetection2026/log_iteration.py
@@ -1,12 +1,16 @@
 from naoth.log import Reader as LogReader
+from naoth.log import Parser
 import argparse
 from vaapi.client import Vaapi 
-import argparse 
 import requests 
 import os
+from pathlib import Path
 
-def save_frame_numbers(log_path, output_path, log_id=log_id):
-    with LogReader(log_path) as reader, open(output_path, "w") as f:
+def save_all_candidates(log_path, log_id):
+    my_parser = Parser()
+    my_parser.register("BallCandidatesTop", "BallCandidates")
+
+    with LogReader(log_path, my_parser) as reader, open(f"data/logs/{log_id}_ball_candidates.txt", "w") as f:
 
         for frame in reader.read():
             f.write(f"Frame: {log_id}_000{frame.number}\n")
@@ -16,7 +20,7 @@ def save_frame_numbers(log_path, output_path, log_id=log_id):
                     bc = frame['BallCandidates']
 
                     for patch in bc.patches:
-                        f.write("BallCandidate patch:\n")
+                        f.write("BallCandidateBottom patch:\n")
                         f.write(
                             f"min_x: {patch.min.x}, "
                             f"min_y: {patch.min.y}, "
@@ -24,44 +28,58 @@ def save_frame_numbers(log_path, output_path, log_id=log_id):
                             f"max_y: {patch.max.y}\n"
                         )
                 except Exception as e:
-                    f.write(f"No  BallCandidates: {e}\n")
+                    # f.write(f"No  BallCandidates: {e}\n")
+                    continue
             
             if 'BallCandidatesTop' in frame.get_names():
-                bc_top = frame['BallCandidatesTop']
-                for patch in bc_top.patches:
-                    f.write("BallCandidatesTop patch:\n")
-                    f.write(
-                        f"min_x: {patch.min.x}, "
-                        f"min_y: {patch.min.y}, "
-                        f"max_x: {patch.max.x}, "
-                        f"max_y: {patch.max.y}\n"
-                    )
+                try: 
+                    bc_top = frame['BallCandidatesTop']
+                    for patch in bc_top.patches:
+                        f.write("BallCandidatesTop patch:\n")
+                        f.write(
+                            f"min_x: {patch.min.x}, "
+                            f"min_y: {patch.min.y}, "
+                            f"max_x: {patch.max.x}, "
+                            f"max_y: {patch.max.y}\n"
+                        )
+                except Exception as e:
+                    # f.write(f"No  BallCandidates: {e}\n")
+                    continue
 
             f.write("\n")
 
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    #parser.add_argument("-l", "--log", required=True, help="Path to log file")
-    parser.add_argument("-o", "--output", default="ball_candidates.txt", help="Output txt file")
-
-    args = parser.parse_args()
-
+def download_log(log_path, log_id):
     v_client = Vaapi(
         base_url=os.environ.get("VAT_API_URL"),
         api_key=os.environ.get("VAT_API_TOKEN"),
     )
-    my_log = v_client.logs.get(679)
-    print(my_log.log_path)
+     
+    my_log = v_client.logs.get(log_id)
 
     url = "https://logs.berlin-united.com/" + my_log.log_path
     response = requests.get(url)
 
     # Open a local file in 'write-binary' mode
-    with open(f"{my_log.id}_game.log", "wb") as f:
+    with open(log_path, "wb") as f:
         f.write(response.content)
 
     print("Download complete!")
 
-    save_frame_numbers(args.log, args.output)
+    return log_path
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log_ids", required=True, type=lambda s: s.split(","), help="log id")
+
+    args = parser.parse_args()
+
+    logs_dir = Path(f"data/logs")
+    logs_dir.mkdir(parents=True, exist_ok=True)
+
+    for log_id in args.log_ids:
+        log_path = Path(f"data/logs/{log_id}_game.log")
+        if not log_path.exists():
+            log_path = download_log(log_path, log_id)
+
+        save_all_candidates(log_path, log_id)
