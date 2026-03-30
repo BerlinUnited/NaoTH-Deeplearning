e5af3ccd23e13c7ca819583af6561e7070e09097
Anina Morgner <anina.ambra@gmail.com>
2026-03-30 12:01:57 +0200

    change hint

--- balldetection2026/autolabeling/ball_trainings_pipeline.py
+++ balldetection2026/autolabeling/ball_trainings_pipeline.py
@@ -85,4 +85,4 @@ if __name__ == "__main__":
 
     project_id = get_project_id(v_client, args.log_ids[0], camera)
     print(f"\nTraining complete. To autolabel new images, run:")
-    print(f"  python run_model_yolo.py -c {camera} -m {ziel_projekt}/{ziel_name}/weights/best.pt -p {project_id}")
+    print(f"uv run run_model_yolo.py -c {camera} -m {ziel_projekt}/{ziel_name}/weights/best.pt -p {project_id}")
