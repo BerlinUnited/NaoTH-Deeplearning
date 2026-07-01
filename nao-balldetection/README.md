21f8d78cc1cc1b9903c8c6f1fe46f49e96231fc0
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-07-01 10:33:54 +0900

    fix: make train work

--- nao-balldetection/README.md
+++ nao-balldetection/README.md
@@ -1,2 +1,9 @@
 # Balldetection for Nao v6
 Currently we use mbc_36ksm_finetuned_crop as classifier and mbd_gopen_56k as detector. See the naoth-2020 repo for details.
+
+
+# generate dataset
+./test.sh
+
+# train
+uv run train.py
--- nao-balldetection/train.py
+++ nao-balldetection/train.py
@@ -6,50 +6,97 @@ from sys import exit
 
 # TODO encode dataset into output model name
 import tensorflow as tf
-import toml
+
 from tensorflow import keras as keras
 
-import utility_functions.metrics as metrics_module
-import utility_functions.model_zoo as model_zoo
+
+from models import mbc_36ksm_finetuned_crop
 
 DATA_DIR = Path(Path(__file__).parent.absolute() / "data").resolve()
+batch_size = 256
+epochs = 10000
 
+@keras.saving.register_keras_serializable(name="weighted_binary_crossentropy")
+def weighted_binary_crossentropy(target, output, weights):
+    target = tf.convert_to_tensor(target)
+    output = tf.convert_to_tensor(output)
+    weights = tf.convert_to_tensor(weights, dtype=target.dtype)
 
-def load_model(my_config):
-    if "model_name" in my_config.keys():
-        method_to_call = getattr(model_zoo, my_config["model_name"])
-        model = method_to_call()
-        # TODO how to handle execptions of getattr?
-        return model
-    elif "proceed" in my_config.keys() and my_config["proceed"] is True:
-        # if you use proceed then you have to give in a model
-        print("Loading model " + my_config.model_path)
+    epsilon_ = tf.constant(keras.backend.epsilon(), output.dtype.base_dtype)
+    output = tf.clip_by_value(output, epsilon_, 1.0 - epsilon_)
 
-        toml_metrics = my_config["metrics"]
-        metrics_dict = {}
-        for metrics_name in toml_metrics:
-            reference = getattr(metrics_module, metrics_name)
-            if isfunction(reference):
-                metrics_dict.update({metrics_name: reference})
-            if isclass(reference):
-                metrics_dict.update({metrics_name: reference()})
+    # Compute cross entropy from probabilities.
+    bce = weights[1] * target * tf.math.log(output + epsilon_)
+    bce += weights[0] * (1 - target) * tf.math.log(1 - output + epsilon_)
+    return -bce
 
-        model = tf.keras.models.load_model(my_config.model_path, custom_objects=metrics_dict)
-        return model
-    else:
-        print(
-            "ERROR: No model specified, you have to either specify model_name in the config or set proceed to true and set model_path")
-        exit(1)
+@keras.saving.register_keras_serializable(name="WeightedBinaryCrossentropy")
+class WeightedBinaryCrossentropy:
+    def __init__(
+        self,
+        label_smoothing=0.0,
+        weights=[1.0, 1.0],
+        axis=-1,
+        name="weighted_binary_crossentropy",
+        loss_fn=weighted_binary_crossentropy,
+    ):
+        """Initializes `WeightedBinaryCrossentropy` instance.
 
+        Args:
+          from_logits: Whether to interpret `y_pred` as a tensor of
+            [logit](https://en.wikipedia.org/wiki/Logit) values. By default, we
+            assume that `y_pred` contains probabilities (i.e., values in [0,
+            1]).
+
+          TODO: Check if this might be helpful?
+          label_smoothing: Float in [0, 1]. When 0, no smoothing occurs. When >
+            0, we compute the loss between the predicted labels and a smoothed
+            version of the true labels, where the smoothing squeezes the labels
+            towards 0.5.  Larger values of `label_smoothing` correspond to
+            heavier smoothing.
+
+          axis: The axis along which to compute crossentropy (the features
+            axis).  Defaults to -1.
+          name: Name for the op. Defaults to 'weighted_binary_crossentropy'.
+        """
+        super().__init__()
+        self.weights = weights  # tf.convert_to_tensor(weights)
+        self.label_smoothing = label_smoothing
+        self.name = name
+        self.loss_fn = weighted_binary_crossentropy if loss_fn is None else loss_fn
+
+    def __call__(self, y_true, y_pred):
+        y_pred = tf.convert_to_tensor(y_pred)
+        y_true = tf.cast(y_true, y_pred.dtype)
+        self.label_smoothing = tf.convert_to_tensor(self.label_smoothing, dtype=y_pred.dtype)
+
+        def _smooth_labels():
+            return y_true * (1.0 - self.label_smoothing) + 0.5 * self.label_smoothing
+
+        y_true = tf.__internal__.smart_cond.smart_cond(self.label_smoothing, _smooth_labels, lambda: y_true)
+
+        return tf.reduce_mean(self.loss_fn(y_true, y_pred, self.weights), axis=-1)
+
+    def get_config(self):
+        config = {"name": self.name, "weights": self.weights, "loss_fn": self.loss_fn}
+
+        return dict(list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
 def main(config_name):
-    with open('config.toml', 'r') as f:
-        config_dict = toml.load(f)
 
-    my_config = config_dict[config_name]
-    model = load_model(my_config)
-
-    data_file = str(DATA_DIR / my_config["trainings_data"])
+    model = mbc_36ksm_finetuned_crop()
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        loss=WeightedBinaryCrossentropy(
+            weights=[1.0, 10.0],
+        ),
+        metrics=["accuracy"],
+    )
+    data_file = str(DATA_DIR / "BOTTOM_GO2026.pkl")
     with open(data_file, "rb") as f:
         pickle.load(f)  # skip mean
         x = pickle.load(f)  # x are all input images
@@ -59,11 +106,12 @@ def main(config_name):
         The save callback will overwrite the previous models if the new model is better then the last. Restarting the 
         training will always overwrite the models.
     """
-    filepath = Path(my_config["output_path"]) / (model.name + "_" + Path(my_config["trainings_data"]).stem + ".h5")
+    output_path= "./"
+    filepath = Path(output_path) / (model.name + "_" + Path(data_file).stem + ".h5")
     save_callback = tf.keras.callbacks.ModelCheckpoint(filepath=str(filepath), monitor='loss', verbose=1,
                                                        save_best_only=True, mode='max')
 
-    log_path = Path(my_config["output_path"]) / "logs" / (
+    log_path = Path(output_path) / "logs" / (
             model.name + "_" + str(datetime.now()).replace(" ", "_").replace(":", "-"))
     log_callback = keras.callbacks.TensorBoard(log_dir=log_path, profile_batch=0)
 
@@ -73,13 +121,13 @@ def main(config_name):
     # history = model.fit(x, y, batch_size=args.batch_size, epochs=args.epochs, verbose=1,
     # validation_data=(X_test, Y_test),callbacks=callbacks)
 
-    history = model.fit(x, y, batch_size=my_config["batch_size"], epochs=my_config["epochs"], verbose=1,
+    history = model.fit(x, y, batch_size=batch_size, epochs=epochs, verbose=1,
                         validation_split=0.1,
                         callbacks=callbacks)
-    history_filename = "history_" + model.name + "_" + Path(my_config["trainings_data"]).stem + ".pkl"
+    history_filename = "history_" + model.name + "_" + Path(data_file).stem + ".pkl"
 
     # save history in same folder as model
-    history_filepath = Path(my_config["output_path"]) / history_filename
+    history_filepath = Path(output_path) / history_filename
     with open(str(history_filepath), "wb") as f:
         pickle.dump(history.history, f)
 
