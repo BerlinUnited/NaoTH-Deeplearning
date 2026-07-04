d5b3295f39845d0403f1a167e3b171160c298b6e
Lennart Keller <lennart.keller@hu-berlin.de>
2026-07-04 15:43:46 +0900

    add function description

--- nao-balldetection/get_data.py
+++ nao-balldetection/get_data.py
@@ -1,11 +1,3 @@
-"""
-Set of functions that download patch data in the form
-0/
- non ball patches
-1/
-  ball patches
-"""
-
 import requests
 import os
 import zipfile
@@ -20,6 +12,13 @@ from create_non_ball_patches import create_non_ball_patches
 
 # naodevils labels
 def get_naodevils_data(output_dir=Path("data/naodevils")):
+    """
+    Set of functions that download patch data from naodevils in the form
+    0/
+     non ball patches
+    1/
+      ball patches
+    """
     url = "https://datasets.berlin-united.com/2026_07_02_patches_ruhrbot_devils.zip"
     output_path = output_dir / Path(f"2026_07_02_patches_ruhrbot_devils.zip")
     extraction_path = output_dir / Path(f"naodevils_data")
@@ -51,6 +50,13 @@ def get_naodevils_data(output_dir=Path("data/naodevils")):
 
 # our labels
 def get_patch_data_from_vat_server():
+    """
+    Set of functions that download patch data from vat and labelstudio in the form
+    0/
+     non ball patches
+    1/
+      ball patches
+    """
     # download_annotated_ball_images_labelstudio(
     #     output_dir=Path("data/ball_images/TOP"), event_id=10, camera="TOP"
     # )
@@ -65,13 +71,29 @@ def get_patch_data_from_vat_server():
     #     input_dir=Path("./data/ball_images/BOTTOM"),
     #     output_dir=Path("./data/ball_patches/BOTTOM"),
     # )
-    create_non_ball_patches(
-        input_dir=Path("./data/ball_images/TOP"),
-        output_dir=Path("./data/non_ball_patches/TOP"),
+    # create_non_ball_patches(
+    #     input_dir=Path("./data/ball_images/TOP"),
+    #     output_dir=Path("./data/non_ball_patches/TOP"),
+    # )
+    # create_non_ball_patches(
+    #     input_dir=Path("./data/ball_images/BOTTOM"),
+    #     output_dir=Path("./data/non_ball_patches/BOTTOM"),
+    # )
+
+    # download_annotated_ball_images_labelstudio(
+    #     output_dir=Path("data/ball_images/"), event_id=10, camera="TOP"
+    # )
+    # download_annotated_ball_images_labelstudio(
+    #     output_dir=Path("data/ball_images/"), event_id=10, camera="BOTTOM"
+    # )
+
+    create_ball_patches(
+        input_dir=Path("./data/ball_images/"),
+        output_dir=Path("./data/ball_patches/1.00"),
     )
     create_non_ball_patches(
-        input_dir=Path("./data/ball_images/BOTTOM"),
-        output_dir=Path("./data/non_ball_patches/BOTTOM"),
+        input_dir=Path("./data/ball_images/"),
+        output_dir=Path("./data/ball_patches/0.00"),
     )
 
 
