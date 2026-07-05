b228a5cb09d2408d80787dc94c185f810ba77854
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-07-05 10:39:02 +0900

    chore: update evaluate script to include go26 patches in the evaluation

--- nao-balldetection/evaluate.py
+++ nao-balldetection/evaluate.py
@@ -7,17 +7,30 @@ from tensorflow import keras as keras
 from loader import subtract_mean
 DATA_DIR = Path(Path(__file__).parent.absolute() / "data").resolve()
 
-model = tf.keras.models.load_model('rc26_classification_color_32_training.h5')
+model = tf.keras.models.load_model('rc26_classification_color_32_naodevils_training.h5')
 
 model.summary()
 
-data_file = str(DATA_DIR / "training.pkl")
+data_file = str(DATA_DIR / "naodevils_training.pkl")
 with open(data_file, "rb") as f:
     mean = pickle.load(f)
     pickle.load(f)  # skip input images
     pickle.load(f)  # skip trainings target: [r, x,y,1]
 
-data_file = str(DATA_DIR / "validation.pkl")
+data_file = str(DATA_DIR / "naodevils_validation.pkl")
+with open(data_file, "rb") as f:
+    pickle.load(f)  # skip mean
+    val_x = pickle.load(f)  # x are all input images
+    val_y = pickle.load(f)  # y are the trainings target: [r, x,y,1]
+
+val_x = subtract_mean(val_x, mean)
+val_y_one_hot = keras.utils.to_categorical(val_y, num_classes=2)
+
+
+results  = model.evaluate(val_x, val_y_one_hot, verbose=1)
+print("Test Loss, Test Accuracy:", results)
+
+data_file = str(DATA_DIR / "go26_patches.pkl")
 with open(data_file, "rb") as f:
     pickle.load(f)  # skip mean
     val_x = pickle.load(f)  # x are all input images
